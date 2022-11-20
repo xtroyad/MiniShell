f726ae948c763e1ea529c7501b505d4456bc0783
xtroyad <cristiancalvatroya@gmail.com>
2022-11-20 12:22:16 +0100

    Intento apartado 1

--- myshell.c
+++ myshell.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
+#include "parser.h"
+#include <signal.h>
 
-int main(void){
-    
-    return 0;
+int main(){ 
+    char buf[1024];
+    tline * comando;
+    signal (SIGINT, SIG_IGN);//Ignoramos la señal CTLR+C
+    int pid;
+
+    while(1){
+        printf("msh> ");
+
+        while(fgets(buf, 1024, stdin)){
+            comando = tokenize(buf); 
+            if (comando->commands->argv=="exit"){//cuando tenemos exit or CTLR+D terminamos
+                kill(getpid(),19); //mandamos un SIGSTOP para MyShell
+            }
+            pid = fork();
+            if (pid<0){
+                fprintf(stderr,"Error a la hora de hacer el fork");        
+            }else if(pid>0){
+                execvp(comando->commands->argv[1], comando->commands->argv+1);
+                fprintf(stderr,"Error a la hora de hacer el comando");
+                exit(1);
+            }else{
+                wait(NULL);
+            }
+            
+        }
+
+    }
+
+
 }
+
