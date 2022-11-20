40d24018dca0b10d8e5a04af30e5ce168096a98c
xtroyad <cristiancalvatroya@gmail.com>
2022-11-20 13:16:29 +0100

    Actualizion

--- myshell.c
+++ myshell.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "parser.h"
 #include <signal.h>
+#include <stdlib.h>
 
 int main(){ 
     char buf[1024];
@@ -8,29 +9,30 @@ int main(){
     signal (SIGINT, SIG_IGN);//Ignoramos la señal CTLR+C
     int pid;
 
-    while(1){ 
+    
+    printf("msh> ");
+    while(fgets(buf, 1024, stdin)){
         
-        while(fgets(buf, 1024, stdin)){
-            printf("msh> ");
-            comando = tokenize(buf); 
-            if (comando->commands->argv[0]=="exit"){//cuando tenemos exit or CTLR+D terminamos
-                kill(getpid(),19); //mandamos un SIGSTOP para MyShell
-            }
-            pid = fork();
-            if (pid<0){
-                fprintf(stderr,"Error a la hora de hacer el fork");        
-            }else if(pid==0){
-                execvp(comando->commands->argv[0], comando->commands->argv);
-                fprintf(stderr,"Error a la hora de hacer el comando");
-                exit(1);
-            }else{
-                wait(NULL);
-            }
-            
+        comando = tokenize(buf); 
+        if (comando->commands->argv[0]=="exit"){//cuando tenemos exit or CTLR+D terminamos
+            kill(getpid(),19); //mandamos un SIGSTOP para MyShell
+            exit(1);
         }
-
+        pid = fork();
+        if (pid<0){
+            fprintf(stderr,"Error a la hora de hacer el fork");        
+        }else if(pid==0){
+            execvp(comando->commands->argv[0], comando->commands->argv);
+            fprintf(stderr,"Error a la hora de hacer el comando");
+            exit(1);
+        }else{
+            wait(NULL);
+        }
+        printf("msh> ");
+        
     }
-
+    
+    return 0;
 
 }
 
