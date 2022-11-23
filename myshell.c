ee4d4a7e30a92c250273ae5a2290b0ba29c5fb77
xtroyad <cristiancalvatroya@gmail.com>
2022-11-23 12:30:28 +0100

    Apartado 2 2.3

--- myshell.c
+++ myshell.c
@@ -12,21 +12,28 @@ void manejador(int sig){
 }
 
 int main(){ 
+    //VARIABLES DEL PROGRAMA
     char buf[1024];
     tline * comando;
-    signal (SIGINT, manejador);//Ignoramos la señal CTLR+C
     FILE * pf;
     printf("msh> ");
+
+    signal (SIGINT, manejador);//Ignoramos la señal CTLR+C
+
+
     while(fgets(buf, 1024, stdin)){
         
         comando = tokenize(buf); 
+
         if (comando->ncommands==0) {
             printf("\nmsh> ");
 			continue;
 		}
+
         if (strcmp(comando->commands[0].argv[0],"exit")==0) {//cuando tenemos exit or CTLR+D terminamos
-            kill(getpid(),9); //mandamos un SIGSTOP para MyShell
+            kill(getpid(),9); //mandamos un SIGKILL para MyShell
         }
+
         pid = fork();
         if (pid<0){
             fprintf(stderr,"Error a la hora de hacer el fork\n");        
@@ -47,8 +54,12 @@ int main(){
                 pf=fopen(comando->redirect_error,"w");
 		    }
             //-----------------------------------------------------------------------------------------
-            execvp(comando->commands[0].argv[0], comando->commands->argv);
+            if (pf==NULL){
+                
+            }
+            execvp(comando->commands[0].argv[0], comando-> commands[0].argv);
             fprintf(stderr,"Error a la hora de hacer el comando\n");
+
             exit(1);
         }else{
             wait(NULL);
