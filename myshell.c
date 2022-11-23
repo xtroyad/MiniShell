fa4eb183ff26dedd8f3ea45e06b85f6a06179134
xtroyad <cristiancalvatroya@gmail.com>
2022-11-23 01:57:29 +0100

    Apartado 2 1.2

--- myshell.c
+++ myshell.c
@@ -10,14 +10,12 @@ void manejador(int sig){
 		kill(getpid(), 9);
 	}
 }
+
 int main(){ 
     char buf[1024];
     tline * comando;
-    int pp[2];
-    FILE * fd;
-    char buf[1024];
     signal (SIGINT, manejador);//Ignoramos la señal CTLR+C
-    
+    FILE * pf;
     printf("msh> ");
     while(fgets(buf, 1024, stdin)){
         
@@ -29,27 +27,22 @@ int main(){
         if (strcmp(comando->commands[0].argv[0],"exit")==0) {//cuando tenemos exit or CTLR+D terminamos
             kill(getpid(),9); //mandamos un SIGSTOP para MyShell
         }
-        pipes(pp);
         pid = fork();
         if (pid<0){
             fprintf(stderr,"Error a la hora de hacer el fork\n");        
         }else if(pid==0){
+            //-----------------------------------------------------------------------------------------
+            if (comando->redirect_output != NULL) {
+                fclose(stdout);
+                pf=fopen(comando->redirect_output,"w");
+            }
+            //-----------------------------------------------------------------------------------------
             execvp(comando->commands[0].argv[0], comando->commands->argv);
             fprintf(stderr,"Error a la hora de hacer el comando\n");
             exit(1);
         }else{
-            // //-----------------------------------------------------------------------------------------
-            
-            // if (comando->redirect_input != NULL) {
-            //     printf("redirección de entrada: %s\n", comando->redirect_input);
-            //     close(pp[1]);
-            //     fd=fdopen(pp[0],"r");
-            // }
-            // //-----------------------------------------------------------------------------------------
-            
             wait(NULL);
         }
-        
         printf("\nmsh> ");
         
     }
